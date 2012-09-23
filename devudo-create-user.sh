57a4cb7b85735427dcb88de5f69150e0c2a2ebe0
Jon Pugh <jon@thinkdrop.net>
2012-09-23 11:46:56 -0500

    progress

--- devudo-create-user.sh
+++ devudo-create-user.sh
@@ -2,8 +2,21 @@
 
 USERNAME=$1
 
-# @TODO: non-interactive mode?
-adduser $USERNAME
+# Check for existing username
+# Thanks, http://www.cyberciti.biz/tips/howto-write-shell-script-to-add-user.html
+egrep "^$USERNAME" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+  echo "$USERNAME exists!"
+  exit 1
+fi 
+
+# create password
+PASSWORD=jkl4gh4jhgjui4o4y
+PASSWORD_ENCRYPTED=$(perl -e 'print crypt($ARGV[0], "password")' $PASSWORD)
+
+# create user
+useradd -m -p $PASSWORD_ENCRYPTED $USERNAME
+echo "User $USERNAME has been created!"
 
 # add user to aegir group
 adduser $USERNAME aegir
@@ -12,3 +25,10 @@ adduser $USERNAME www-data
 # create symlink to their hostmaster
 mkdir /home/$USERNAME/.drush
 ln -s /var/aegir/.drush/$USERNAME.devudo.com.alias.drushrc.php /home/$USERNAME/.drush/
+ln -s /var/aegir/.drush/server_localhost.alias.drushrc.php /home/$USERNAME/.drush/
+ln -s /var/aegir/.drush/platform_devmaster6x19.alias.drushrc.php /home/$USERNAME/.drush/
+
+# @TODO: as the user, run hosting setup.
+ 
+
+
