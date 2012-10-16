e54d1605df861e619f6b133c4d7492b093ae0cf1
Jon Pugh <jon@thinkdrop.net>
2012-10-16 12:17:36 -0500

    fixing detection of username

--- devudo-delete-user.sh
+++ devudo-delete-user.sh
@@ -2,6 +2,11 @@
 
 USERNAME=$1
 
+if [ $USERNAME == '' ]; then
+  echo "You must provide a username."
+  exit 1
+fi
+
 # Check for existing username
 # Thanks, http://www.cyberciti.biz/tips/howto-write-shell-script-to-add-user.html
 egrep "^$USERNAME" /etc/passwd >/dev/null
